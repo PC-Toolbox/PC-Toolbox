4e918b7cf4d2e37f8c92fc8f2159db01b2887b8b
Byron <53088136+byronbytes@users.noreply.github.com>
2020-10-14 08:34:00 -0400

    PC Toolbox Troubleshooter v1.2

--- PC Toolbox Troubleshooter.py
+++ PC Toolbox Troubleshooter.py
@@ -7,7 +7,7 @@ init()
 from colorama import Fore, Back, Style
 
 os.system("title PC Toolbox Troubleshooter")
-print(Fore.LIGHTRED_EX + 'PC Toolbox Troubleshooter only supports versions of PC Toolbox 0.7 and up')
+print(Fore.LIGHTRED_EX + 'This Troubleshooter only supports versions of PC Toolbox 0.7 and up')
 print(' ')
 print(' ')
 print(Fore.WHITE + '')
@@ -26,38 +26,43 @@ print('[4] Credits')
 print(' ')
 print('[5] Close Troubleshooter')
 
-
+print(Fore.LIGHTWHITE_EX + ' ')
 choice = int(input()) 
 
 
 
-if choice ==1 :
+if choice == 1 :
  print(Fore.LIGHTRED_EX + ' ')
- print('This is going to repair the installation of PC Toolbox by re-running the installer.')
- print(' ')
+ print('This is going to repair the installation of PC Toolbox by re-installing the app.')
  print('To continue, please close all processes of PC Toolbox.')
  print(' ')
- #lol i just need an setup.exe and some advanced maths to do this
+ print('Currently running installer. . .')
+ print(' ')
  print(Fore.LIGHTYELLOW_EX + 'The application will now close.')
  os.system('pause')
  sys.exit()
+ #lol i just need an setup.exe and some advanced maths to do this
  
 
 
-if choice ==2 :
+if choice == 2 :
  print(Fore.LIGHTRED_EX + ' ')
- print('This will uninstall PC Toolbox, and PC Toolbox Troubleshooter, all registry keys made by PC Toolbox will also be removed')
+ print('This will uninstall all PC Toolbox executables, scripts, and all links to the application')
+ print('If you decided that this was an accident, close the troubleshooter and it will not uninstall.')
  print('')
- print('To uninstall, please close all processes of PC Toolbox.')
- #lol idk how to do this part
- print(Fore.LIGHTYELLOW_EX + 'The application will now close.')
+ os.system('pause')
+ print(Fore.LIGHTWHITE_EX + 'Running uninstaller...')
+ print('')
+ print(Fore.LIGHTYELLOW_EX + 'The application will now close. Thank you for your time on the app!')
  os.system('pause')
  sys.exit()
+#lol idk how to do this part
 
-
-if choice ==3:
+if choice == 3:
  print(Fore.LIGHTRED_EX + ' ')
  print ('Please fill out an issue form on GitHub, if you do not have an account please create one.')
+ print('')
+ print('Or use our beta software, DirectReport, which is currently avalible in later builds of PC Toolbox.')
  webbrowser.open('https://github.com/byronbytes/PC-Toolbox/issues/new', new=2)
  print(Fore.LIGHTYELLOW_EX + 'The application will now close.')
  os.system('pause')
@@ -66,20 +71,36 @@ if choice ==3:
     
 
 
-if choice ==4 :
+if choice == 4 :
  print(Fore.LIGHTRED_EX + ' ')
  print('This version of PC Toolbox Troubleshooter was made by:')
  print(Fore.BLUE + 'byronbytes (byron#0289) ')
  print('')
  print(Fore.RED + 'localtechnerd (Side#6776)')
+ print('')
+ print('')
+ print('')
+ print(Fore.LIGHTWHITE_EX + 'PC Tolbox Troubleshooter is currently on v1.2')
+
  print(Fore.LIGHTYELLOW_EX + ' ')
- os.system('pause')
  print(Fore.LIGHTYELLOW_EX + 'The application will now close.')
  os.system('pause')
  sys.exit()
 
-if choice ==5 :
- print(Fore.LIGHTYELLOW_EX + 'The application will now close upon user request.')
+if choice == 5 :
+ print(Fore.LIGHTYELLOW_EX + 'The troubleshooter  will now close upon user request.')
+ os.system('pause')
+ sys.exit()
+
+
+
+#my old channel.... rip byrontall / themgamer1200 (TheMattyGamer)
+if choice ==1200 :
+ print('You found a secret number combo!')
+ print(' ')
+ print('Hey guys its the m gamer 1200, with another minecraft video!!!!11!')
+ print(Fore.LIGHTYELLOW_EX + ' ')
+ print(Fore.LIGHTYELLOW_EX + 'The application will now close.')
  os.system('pause')
  sys.exit()
 
